8447587497df04afc8e507fafddae878cbf9f23b
holgater <richardjholgate@gmail.com>
2016-07-08 17:38:16 -0700

    Added basic comments

--- ftp_client.java
+++ ftp_client.java
@@ -12,39 +12,52 @@ public class ftp_client {
     String username;
     String password;
     
+    //shouldn't be more than 3 args - error
     if(args.length > 3)
       System.out.println("RTFM!"); //Yes, I know there is no manual
     
+    //shouldn't be fewer than 1 args - error
     if(args.length < 1) {
       System.out.print("Server: ");
       server = console.readLine();
     }
+    //otherwise procede
+    //server name is the first arg
     else
       server = args[0];
     
+    //if not provided...
     if(args.length < 2) {
+      //get username from prompt
       System.out.print("Username: ");
       username = console.readLine();
     }
     else
+      //othewise get username from args provided
       username = args[1];
     
+    //if not provided...
     if(args.length < 3) {
+      //get username from prompt
       System.out.print("Password: ");
       password = new String(console.readPassword());
     }
     else
+      //otherwise get username from args provided
       password = args[2];
     
     try {
+      //try connecting
       ftpClient.connect(server, port);
       showServerReply(ftpClient);
 
+      //couldn't connect - error
       if(!FTPReply.isPositiveCompletion(ftpClient.getReplyCode())) {
         System.out.println("Could not connect");
         System.exit(1);
       }
 
+      //try logging in
       boolean login = ftpClient.login(username, password);
       showServerReply(ftpClient);
       if(!login) {
@@ -55,12 +68,15 @@ public class ftp_client {
         showServerReply(ftpClient);
     }
     catch(Exception ex) {
+    	//error catch
       System.out.println("Something went wrong :(");
     }
   }
   
+  //get a reply from the server
   private static void showServerReply(FTPClient ftpClient) {
     String[] replies = ftpClient.getReplyStrings();
+    //if there are messages, display each one in order
     if (replies != null && replies.length > 0)
       for (String reply : replies)
         System.out.println("SERVER: " + reply);
