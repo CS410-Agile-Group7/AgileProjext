d617005cf10a117463d136ad4717ea3d174ddc6b
holgater <richardjholgate@gmail.com>
2016-08-02 15:23:16 -0700

    Added basic tests for list local - make sure to add command line argument info in test configurations

--- src/ftp_clientTest.java
+++ src/ftp_clientTest.java
@@ -1,21 +1,29 @@
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class ftp_clientTest {
 
 	ftp_client client;
+	String localDir;
 	
 	@Before
 	public void setup() {
 		//Setup client
-		client = new ftp_client();
-    //call main
-		client.main(null);
+		localDir = System.getProperty("user.dir");
 	}
 	
 	@Test
 	public void listLocal() {    
-
+		//list local current directory
+		assertTrue(ftp_client.listLocal(localDir));
+		//list local dirrectory one up
+		assertTrue(ftp_client.listLocal(localDir.concat("/..")));
+		//list local root directory
+		assertTrue(ftp_client.listLocal("/"));
+		//list directory that doesn't work -- should return FALSE
+		assertFalse(ftp_client.listLocal(localDir.concat("asdfasdfasdfadf")));
 	}
 
 }
